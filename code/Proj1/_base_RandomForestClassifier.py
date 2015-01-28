388b2fa6a6959f0d4468e34ec25526cb851ceb61
fahad623 <fahad2191@gmail.com>
2015-01-27 19:35:31 -0600

    a

--- code/Proj1/_base_RandomForestClassifier.py
+++ code/Proj1/_base_RandomForestClassifier.py
@@ -9,12 +9,14 @@ from sklearn.metrics import log_loss
 predict_method = 'proba'
 
 def make_best_classifier():
-    return RandomForestClassifier(n_jobs = 7, n_estimators = 130), predict_method
+    return RandomForestClassifier(n_jobs = 7, n_estimators = 65, max_depth = 3), predict_method
 
 def train_base_clf(pp):
     clf = make_best_classifier()[0]
-    n_estimators_range = np.arange(100, 150, 10)
-    param_grid = dict(n_estimators = n_estimators_range)
+    n_estimators_range = np.arange(40, 70, 5)
+    max_depth_range = np.arange(2, 4, 1)
+
+    param_grid = dict(n_estimators = n_estimators_range, max_depth = max_depth_range)
     clf, bp, bs = cross_val.fit_clf(clf, pp.X_train, pp.Y_train, param_grid, 'log_loss')
     output_csv.write_test_csv(clf.__class__.__name__, pp.df_output_test, clf.predict_proba(pp.X_test)[:,1])
     output_csv.write_gs_params_base(clf.__class__.__name__, bp, bs, 
